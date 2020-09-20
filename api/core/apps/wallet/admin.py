10edd097ec1c596b745e0a2cae4ffca25dfbd3bb
sonerayberk <soner.ayberk@gmail.com>
2020-09-20 20:34:51 +0300

    Refill amount model

--- api/core/apps/wallet/admin.py
+++ api/core/apps/wallet/admin.py
@@ -7,3 +7,9 @@ from core.apps.wallet import models as wallet_models
 class WithdrawRequestAdmin(admin.ModelAdmin):
     list_display = ("account", "amount", "card_number", "status", "is_active", )
     list_filter = ("status", "is_active",)
+
+
+@admin.register(wallet_models.Refill)
+class RefillAdmin(admin.ModelAdmin):
+    list_display = ("account", "amount", "multiplier", "status", "created", )
+    list_filter = ("status",)
