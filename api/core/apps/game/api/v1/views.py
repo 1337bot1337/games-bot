a7f561ce5b067585639ac5ef77f5bd22f4c9d1e8
sonerayberk <soner.ayberk@gmail.com>
2020-09-20 19:58:20 +0300

    Update games-list API endpoint

--- api/core/apps/game/api/v1/views.py
+++ api/core/apps/game/api/v1/views.py
@@ -3,25 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.apps.game import services as game_services
-from core.apps.game.utils import GAME_LIST
-from .serializers import StartGameSerializer
 
 
 class GamesListAPIView(APIView):
     def get(self, request):
-        response_items = [{key: GAME_LIST[key]} for key in GAME_LIST.keys()]
-        return Response(data=response_items, status=status.HTTP_200_OK)
-
-
-class StartGameAPIView(APIView):
-    serializer_class = StartGameSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        game_data = game_services.start_game(
-            tg_id=serializer.validated_data["tg_id"],
-            game_id=serializer.validated_data["game_id"],
-            amount=serializer.validated_data["amount"],
-        )
-        return Response(data=game_data, status=status.HTTP_200_OK)
+        return Response(data=game_services.get_games(), status=status.HTTP_200_OK)
