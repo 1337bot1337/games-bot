d24dd34cde92824030f5c571c7b91bfcf21b0594
cyace84 <cyace84@gmail.com>
2020-10-23 07:09:12 +0300

    Dockerfile bot

--- api/core/apps/account/api/v1/urls.py
+++ api/core/apps/account/api/v1/urls.py
@@ -8,4 +8,5 @@ app_name = 'core.apps.account'
 
 urlpatterns = [
     path('user/', views.RegisterUserSourceAPIView.as_view(), name='register-source'),
+    path('<int:tg_id>/', views.UserCheckAPIView.as_view(), name='check-user')
 ]
--- api/core/apps/account/services.py
+++ api/core/apps/account/services.py
@@ -6,4 +6,3 @@ from . import models as account_models
 def register_user_source(*, tg_id: int, source: str) -> typing.Optional["account_models.TelegramAccount"]:
     if not account_models.TelegramAccount.objects.filter(tg_id=tg_id).exists():
         return account_models.TelegramAccount.objects.create(tg_id=tg_id, source=source)
-
